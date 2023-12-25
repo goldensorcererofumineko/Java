3a9cf81acb965891c8523b407f4ddbe1892bcfae
goldensorcererofumineko <143801333+goldensorcererofumineko@users.noreply.github.com>
2023-12-25 23:56:42 +0900

    java

--- Sample.java
+++ Sample.java
@@ -574,3 +574,23 @@ class Sample41 {
         }
     }
 }
+
+class Calculation {
+    public String operator = "+";
+    public int num1 = 10;
+    public int num2 = 5;
+}
+
+class Execution {
+    public static void main(String[] args) {
+    Application app = new Application();
+    String result = app.calc.num1 + app.calc.operator + app.calc.num2 + "=" + (app.calc.num1 + app.calc.num2);
+    System.out.println(app.msg);
+    System.out.println(result);
+    }
+}
+
+class Application {
+  public Calculation calc = new Calculation();
+  public String msg = "計算結果表示";
+}
