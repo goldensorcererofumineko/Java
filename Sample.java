992d578bd2d23df0243aa858f61ca8bb6f6934ca
goldensorcererofumineko <143801333+goldensorcererofumineko@users.noreply.github.com>
2024-03-16 22:14:29 +0900

    ハノイ

--- Sample.java
+++ Sample.java
@@ -2339,3 +2339,42 @@ class Application3 {
       return 1;
   }
 }
+/* メソッドは自身を呼べる */
+
+/* ハノイの塔 
+ *  * 円盤 4枚の時
+ *
+ *  1     | I |              |              |
+ *  2    |  J  |             |              |
+ *  3   |   K   |            |              |
+ *  4  |    L    |           |              |
+ *    --------------  --------------  --------------
+ *          A                B              C
+ *        start             tmp            gaol
+ *
+ *  1       |                |            | I |
+ *  2       |                |           |  J  |
+ *  3       |                |          |   K   |
+ *  4       |                |         |    L    |
+ *    --------------  --------------  --------------
+ *          A                B              C
+ *        start             tmp            gaol
+*/
+
+class Hanoi {
+  public char start = 'A'; // スタートの棒の変数
+  public char tmp = 'B'; // 一時移動先の棒の変数
+  public char goal = 'C'; // ゴールの棒の変数
+  public char[] disks = {'I', 'J', 'K', 'L'}; // 円盤が 4 枚時の名前
+  public int moveCount = 0; // 移動回数
+
+  // 以下に円盤を移動する処理メソッドを追加する
+
+}
+
+class Execution8 {
+  public static void main(String[] args) {
+      // 以下に出力処理をコーディングする
+
+  }
+}
